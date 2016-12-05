b73f09c4893e0e78e9d539779d3238102192c8c9
JulianSansat <kerarchie@yahoo.com.ar>
2016-12-04 22:49:23 -0300

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'fierce-badlands-86576.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'seg-gamedu.herokuapp.com' }
 end
